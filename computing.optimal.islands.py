dbac73af18a231a135a4ae504a6b4c70aa0e5bd2
Tinkara Žitko <45061270+TInkara25@users.noreply.github.com>
2020-11-27 12:08:31 +0100

    popravljena funkcija povezave

--- computing.optimal.islands.py
+++ computing.optimal.islands.py
@@ -15,10 +15,10 @@ def nakljucne_tocke(n, k):
         i += 1
     return randPoints
 
-#tocke = nakljucne_tocke(10,10)
-#print(tocke)
+tocke = nakljucne_tocke(10,10)
+print(tocke)
 
-randPoints = [[6.795, 6.982], [3.744, 7.532], [6.621, 1.728], [8.745, 9.22], [3.676, 3.392], [4.494, 8.904], [9.794, 8.195], [2.055, 3.823], [2.384, 1.969], [6.45, 8.747]]  
+#randPoints = [[6.795, 6.982], [3.744, 7.532], [6.621, 1.728], [8.745, 9.22], [3.676, 3.392], [4.494, 8.904], [9.794, 8.195], [2.055, 3.823], [2.384, 1.969], [6.45, 8.747]]  
 
 def najvisja_tocka(randPoints):
     #določi točko, ki ima največjo y koordinato
@@ -26,34 +26,31 @@ def najvisja_tocka(randPoints):
         najvisja_tocka = max(randPoints,key=lambda item: item[1])            
     return najvisja_tocka
 
-najvisja = najvisja_tocka(randPoints)
+najvisja = najvisja_tocka(tocke)
 print(najvisja)
-
-randPoints = randPoints.remove(najvisja_tocka)
-print(randPoints)
 
 def povezave(randPoints):
     #določi seznam vseh možnih povezav med dvema točkama na množici točk, brez najvišje
-    randPoints = randPoints.remove(najvisja_tocka)
+    x = najvisja_tocka(randPoints)
+    randPoints.remove(x)
     seznam = []
-    for i in randPoints:
-        for j in (randPoints.remove(i)):
-            if (i,j) not in seznam:
-                seznam.append((i,j))
+    for i in range(0, len(randPoints)):
+        a = randPoints[i]
+        for j in range(i, len(randPoints)):
+            b = randPoints[j]
+            seznam.append((a,b))
     return seznam
 
-print(povezave(randPoints))
-print(randPoints)
+print(povezave(tocke))
+print(len(povezave(tocke)))
 
-def trikotnik(a,b,c):
+#def trikotnik(a,b,c):
     #določi vse legalne trikotnike na najvišji točki in povezavah
     
 
-def ploscina_trikotnika(a, b,c):
+def ploscina_trikotnika(a,b,c):
     #ploščina trikotnika z oglišči a, b in c
     x=[a[0],b[0],c[0]]
     y=[a[1],b[1],c[1]]
     area=0.5*( (x[0]*(y[1]-y[2])) + (x[1]*(y[2]-y[0])) + (x[2]*(y[0]-y[1])) )
     return area
-
-print(ploscina_trikotnika(a, b,c))
